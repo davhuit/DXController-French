44825312d5fc3228bd262e9febc38ed7e6231613
davhuit <hieitf76@gmail.com>
2026-09-19 00:03:52 +0200

    Modification pour que le fichier DeusEx.u soit automatiquement renommé en DeusEx.frt_u à la fin du build

--- sync-and-build.sh
+++ sync-and-build.sh
@@ -211,7 +211,7 @@ fi
 
 # Pass 3: produce the French-patch-named copy for deployment. The FR
 # translation patch expects DeusEx.frt_u instead of DeusEx.u
-# so we rename DeusEx.u t with the expected name (DeusEx.frt_u).
+# so we rename DeusEx.u with the expected name (DeusEx.frt_u).
 mv "$BUILD_DIR/System/DeusEx.u" "$BUILD_DIR/System/DeusEx.frt_u"
 
 echo "sync-and-build: ok"
