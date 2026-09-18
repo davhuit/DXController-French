b6c926393936c75368add02c1769961654e9b0c4
Davhuit <hieitf76@gmail.com>
2026-09-18 04:40:00 +0200

    Sync avec la nouvelle version US du mod + fusion FR

--- launcher/gen-version.ps1
+++ launcher/gen-version.ps1
@@ -34,8 +34,8 @@ function Parse-TagInts {
     return $ints
 }
 
-$ints = @(0, 0, 0, 0)
-$displayStr = 'dev-untagged'
+$ints = @(1, 6, 1, 0)
+$displayStr = 'Davhuit FR 1.6.1'
 
 if ($env:LAUNCHER_VERSION) {
     # Explicit override (CI sets this from github.ref_name on tag builds).
